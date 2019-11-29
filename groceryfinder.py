572d4d4eeab4363da7cf66e58b3ad224414e7577
Liam Amadio <liamamadio@gmail.com>
2019-11-28 19:19:37 -0600

    starting fresh

--- groceryfinder.py
+++
@@ -1,8 +0,0 @@
-isleList = [["ice_cream","pizza"],["veg","rice"]]
-#input list
-inputList = []
-numList = int(input("How many items do you have in your list?"))
-for n in range(numList):
-    items = input("Enter item")
-    inputList.append(items)
-print(inputList)
