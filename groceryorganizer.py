59fab6ed0aaf7e6a99cbd06168aec1e1b5d9b117
Liam Amadio <liamamadio@gmail.com>
2019-12-16 10:16:41 -0600

    complete redo

--- groceryorganizer.py
+++ groceryorganizer.py
@@ -2,15 +2,14 @@
 #November 28, 2019
 #This program takes a shopping list as input and sorts the items based on what isle they are in
 from isle_items import isle_items
-shopping_list = ["beer", "club soda", "bread", "rice", "vegetables", "pickles", "honey"]
+shopping_list = ["cold beer", "club soda", "wheat bread", "rice", "merlot wine", "penut butter and jelly", "spicy sauce", "fish"]
+#inventory = {'beer':1, 'soda':1, 'bread':2, 'rice':2, 'pickles':3, 'honey':3}
+inventory = isle_items
+for isle in range(1,3):
+    print("Isle",isle,"\n-----")
+    for key in inventory:
+        for item in shopping_list:
+            if key in item and inventory[key] == isle:
+                print(item)
 
-x = 1
-for j in range(len(shopping_list)):
-    for i in shopping_list:
-        if isle_items[i] == x:
-            print("Isle", x,"\n-----")
-            break
-    for i in shopping_list:
-        if isle_items[i] == x:
-            print(i)
-    x = x + 1
+
